23c0f6a498e845e3fd31c50a44340fc6c0ea8c3a
Andrea Leopardi <an.leopardi@gmail.com>
2015-05-28 00:09:15 +0200

    Setup the structure for integration tests
    The tests in the `integration_test/` directory will be run when the Mix
    environment is `:integration`; I also added a `mix test.all` task (taken pretty
    much verbatim from Ecto) to run both unit as well as integration tests.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule MarcoPolo.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+
+     build_per_environment: false,
+
+     # Testing
+     test_paths: if(Mix.env == :integration, do: ["integration_test"], else: ["test"]),
+     aliases: ["test.all": &test_all/1],
+     preferred_cli_env: ["test.all": :test]]
   end
 
   def application do
@@ -16,5 +23,20 @@ defmodule MarcoPolo.Mixfile do
 
   defp deps do
     []
+  end
+
+  # This beauty is taken almost verbatim from the mix.exs file of the Ecto
+  # project (https://github.com/elixir-lang/ecto/blob/v0.11.3/mix.exs).
+  defp test_all(args) do
+    args = if IO.ANSI.enabled?, do: ["--color"|args], else: ["--no-color"|args]
+    Mix.Task.run "test", args
+
+    {_, res} = System.cmd("mix", ["test"|args],
+                          into: IO.binstream(:stdio, :line),
+                          env: [{"MIX_ENV", "integration"}])
+
+    if res > 0 do
+      System.at_exit(fn _ -> exit({:shutdown, 1}) end)
+    end
   end
 end
