1e507f86ac097e0ce98f9407998b18f8b055a7d8
Andrea Leopardi <an.leopardi@gmail.com>
2015-07-01 15:32:32 +0200

    Add documentation to the `MarcoPolo.RID` module

--- lib/marco_polo/rid.ex
+++ lib/marco_polo/rid.ex
@@ -1,3 +1,24 @@
 defmodule MarcoPolo.RID do
+  @moduledoc """
+  Struct that represents an OrientDB record id (RID).
+
+  OrientDB identifies records with a unique id, called the record id (RID). A
+  RID consists of the id of the cluster the record is and the position of the
+  record in the cluster. The fields for the `MarcoPolo.RID` struct represent
+  exactly this:
+
+    * `:cluster_id` - the id of the cluster
+    * `:position` - the position in the cluster
+
+  For more information on RIDs, refer to the [OrientDB
+  docs](http://orientdb.com/docs/2.0/orientdb.wiki/Tutorial-Record-ID.html).
+
+  """
+
+  @type t :: %__MODULE__{
+    cluster_id: non_neg_integer,
+    position: non_neg_integer,
+  }
+
   defstruct [:cluster_id, :position]
 end
