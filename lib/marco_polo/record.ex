ce47901654d874ec6c196abbfcd39f5ba6f73c3a
Andrea Leopardi <an.leopardi@gmail.com>
2015-07-02 23:53:57 +0200

    Add a type for the MarcoPolo.Record struct

--- lib/marco_polo/record.ex
+++ lib/marco_polo/record.ex
@@ -1,4 +1,10 @@
 defmodule MarcoPolo.Record do
+  @type t :: %__MODULE__{
+    class: nil | String.t,
+    version: non_neg_integer,
+    fields: %{}
+  }
+
   defstruct class: nil,
             version: nil,
             fields: %{}
