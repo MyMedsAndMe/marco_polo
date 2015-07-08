ead21d2f6530901c28c7f8a2ce7c5111bc980aa2
Andrea Leopardi <an.leopardi@gmail.com>
2015-07-09 00:17:45 +0200

    Remove some unused operation codes

--- lib/marco_polo/protocol.ex
+++ lib/marco_polo/protocol.ex
@@ -391,26 +391,20 @@ defmodule MarcoPolo.Protocol do
   defp req_code(:db_close),                          do: 5
   defp req_code(:db_size),                           do: 8
   defp req_code(:db_countrecords),                   do: 9
-  defp req_code(:datacluster_copy),                  do: 14
-  defp req_code(:datacluster_lh_cluster_is_used),    do: 16
-  defp req_code(:record_metadata),                   do: 29
   defp req_code(:record_load),                       do: 30
+  defp req_code(:record_load_if_version_not_latest), do: 44
   defp req_code(:record_create),                     do: 31
   defp req_code(:record_update),                     do: 32
   defp req_code(:record_delete),                     do: 33
   defp req_code(:record_copy),                       do: 34
-  defp req_code(:record_clean_out),                  do: 38
   defp req_code(:positions_floor),                   do: 39
   defp req_code(:command),                           do: 41
   defp req_code(:positions_ceiling),                 do: 42
-  defp req_code(:record_load_if_version_not_latest), do: 44
   defp req_code(:tx_commit),                         do: 60
   defp req_code(:db_reload),                         do: 73
   defp req_code(:push_record),                       do: 79
   defp req_code(:push_distrib_config),               do: 80
-  defp req_code(:db_copy),                           do: 90
   defp req_code(:replication),                       do: 91
-  defp req_code(:cluster),                           do: 92
   defp req_code(:db_transfer),                       do: 93
   defp req_code(:db_freeze),                         do: 94
   defp req_code(:db_release),                        do: 95
