24bbdc6ee469fb4fb97f9246bc585d983fcd765e
Andrea Leopardi <an.leopardi@gmail.com>
2015-07-18 23:07:26 +0200

    Refine Protocol.parse_connection_resp/2

--- lib/marco_polo/protocol.ex
+++ lib/marco_polo/protocol.ex
@@ -119,10 +119,6 @@ defmodule MarcoPolo.Protocol do
     :incomplete
   end
 
-  def parse_connection_resp(data, connection_op) do
-    parse_resp(connection_op, data, nil)
-  end
-
   @doc """
   Parses the response to a given operation.
 
@@ -130,9 +126,19 @@ defmodule MarcoPolo.Protocol do
   to determine the structure of the response. `schema` is passed down to the
   record deserialization (in case there are records) in order to decode possible
   property ids.
+
+  The return value is a three-element tuple:
+
+    * the first element is the session id of the response.
+    * the second element is the actual response (which can be `{:ok, something}`
+      or `{:error, reason}`.
+    * the third element is what remains of `data` after parsing it according to
+      `op_name`.
+
   """
   @spec parse_resp(atom, binary, Dict.t) ::
-    {sid :: non_neg_integer, resp :: term, rest :: binary} | :incomplete
+    :incomplete |
+    {integer, {:ok, term} | {:error, term}, binary}
   def parse_resp(op_name, data, schema) do
     case parse_header(data) do
       :incomplete ->
@@ -154,6 +160,18 @@ defmodule MarcoPolo.Protocol do
             {sid, {:error, error}, rest}
         end
     end
+  end
+
+  @doc """
+  Parses the response to a connection operation.
+
+  Works very similarly to `parse_resp/3`.
+  """
+  @spec parse_connection_resp(binary, atom) ::
+    :incomplete |
+    {integer, {:ok, term} | {:error, term}, binary}
+  def parse_connection_resp(data, connection_op) do
+    parse_resp(connection_op, data, %{})
   end
 
   defp parse_header(@ok <> <<sid :: int, rest :: binary>>),
